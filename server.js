9edbf7c21c34e0ce91e87e6ddd28d6933acbfe1d
MD NAZIM <131196179+Nazim452@users.noreply.github.com>
2024-02-07 21:14:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,17 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from 'path';
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //database conn
 connectDB();
-
+//ES Mode
 //rest object
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middleware
